76365a94349be96b7681301e4c4b357924921735
Quang Nguyen <quanguyendg@gmail.com>
2023-04-13 21:12:04 -0500

    clean up

--- src/huffmantree.py
+++ src/huffmantree.py
@@ -1,16 +1,16 @@
 import BSTClass
 from collections import defaultdict
 
-def encode_text():
+def create_dictionary():
   text = input()
-  text_list =  list(text)
-
+  text_list = list(text)
   text_dict = defaultdict(int)
 
   for c in text_list:
     text_dict[c] += 1
-
-
   return text_dict
 
-print(encode_text())
+print(create_dictionary())
+
+def create_node(c):
+  return
