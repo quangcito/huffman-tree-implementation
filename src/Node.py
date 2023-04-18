72d83f25a69411b65b677ebbdc61c8e26dcab342
Quang Duong Nguyen <qnguyen2@macalester.edu>
2023-04-17 21:00:13 -0500

    add huffman_node

--- src/Node.py
+++ src/Node.py
@@ -1,11 +1,11 @@
-class BST:
+class Node:
   """Implements a typical Binary Search Tree that does no balancing. It also includes
   several general binary tree algorithms, just so you can see them."""
 
 
   def __init__(self, character, occurences, left = None, right = None):
     self.character = character
-    self.occurence = occurences
+    self.occurences = occurences
     self.left = left
     self.right = right
     self.code = None
--- src/huffmantree.py
+++ src/huffmantree.py
@@ -1,4 +1,4 @@
-import Node
+from Node import Node
 from collections import defaultdict
 
 def create_dictionary(text):
@@ -8,7 +8,7 @@ def create_dictionary(text):
   for c in text_list:
     text_dict[c] += 1
 
-  text_dict = dict(sorted(text_dict.items(), key=lambda x:x[1]))
+  text_dict = dict(sorted(text_dict.items(), key=lambda x:x[1], reverse=True))
   return text_dict
 
 print(create_dictionary("Hello"))
@@ -16,8 +16,27 @@ print(create_dictionary("Hello"))
 def huffman_encode(text):
   huffman_dict = create_dictionary(text)
   nodes = []
+  huffman_nodes = []
   for c in huffman_dict.keys():
     nodes.append(Node(c, huffman_dict.get(c)))
+
+  while not nodes and len(nodes) > 0:
+    left_node = nodes.pop()
+    right_node = nodes.pop()
+
+    left_node.code = 0
+    right_node.code = 1
+
+    combined_node = Node(left_node.character + right_node.character, left_node.occurences + right_node.occurences, left_node, right_node)
+    huffman_nodes.append(combined_node)
+
+  huffman_nodes.append(nodes[0])
+  
   return nodes
 
 print(huffman_encode("hello"))
+
+for node in huffman_encode("hello"):
+  print(node.character, node.occurences)
+
+print(huffman_encode("hello").pop().character)
